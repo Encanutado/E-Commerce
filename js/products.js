16bc4a09d00bd1bef9917b77b2763fe5e3df6f5e
Federico Andrés Pereira Benavente <f_p_b@hotmail.es>
2021-08-20 09:30:19 -0300

    parte 2 del obligatorio hecha
    Parte 2 fetch y mostrar el listado en html hecho.

--- js/products.js
+++ js/products.js
@@ -2,5 +2,36 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+     getJSONData(PRODUCTS_URL)
+    .then((result) => result.data)
+    .then((productdata) => showProductsList(productdata));
 });
+
+
+
+function showProductsList(productdata){
+
+for(let i = 0; i < productdata.length; i++){
+        let producto = productdata[i];
+
+document.getElementById("produto"). innerHTML += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + producto.imgSrc + `" alt="` + producto.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ producto.name +`</h4>
+                            <small class="text-muted">` + producto.soldCount + ` artículos</small>
+                        </div>
+                            <small class="text-muted">` + producto.cost + " " + producto.currency + ` </small>
+                        <p class="mb-1">` + producto.description + `</p>
+                        
+                    </div>
+                </div>
+            </a>
+            `
+}
+}
+
