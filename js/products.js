359888611b9638606f3247800f836fc7818f8a2f
Federico Andrés Pereira Benavente <f_p_b@hotmail.es>
2021-09-03 21:12:49 -0300

    Arreglo de los botones.
    Agrego los botones que van a servir para añadir las funcionalidades de limpiar y filtrar.

--- js/products.js
+++ js/products.js
@@ -2,7 +2,7 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-     getJSONData(PRODUCTS_URL)
+  getJSONData(PRODUCTS_URL)
     .then((result) => result.data)
     .then((productdata) => mostrarProductos(productdata));
 });
@@ -10,43 +10,56 @@ document.addEventListener("DOMContentLoaded", function (e) {
 var actualListadoProductos = [];
 var criterioActual = [];
 
-function mostrarProductos(productdata){
+function mostrarProductos(productdata) {
+  let productosAMostrar = "";
 
-    let productosAMostrar = "";
+  for (let i = 0; i < productdata.length; i++) {
+    let producto = productdata[i];
 
-for(let i = 0; i < productdata.length; i++){
-        let producto = productdata[i];
-
-productosAMostrar += `
+    productosAMostrar +=
+      `
             <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
-                        <img src="` + producto.imgSrc + `" alt="` + producto.description + `" class="img-thumbnail">
+                        <img src="` +
+      producto.imgSrc +
+      `" alt="` +
+      producto.description +
+      `" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ producto.name +`</h4>
-                            <small class="text-muted">` + producto.soldCount + ` artículos</small>
+                            <h4 class="mb-1">` +
+      producto.name +
+      `</h4>
+                            <small class="text-muted">` +
+      producto.soldCount +
+      ` artículos</small>
                         </div>
-                            <small class="text-muted">` + producto.cost + " " + producto.currency + ` </small>
-                        <p class="mb-1">` + producto.description + `</p>
+                            <small class="text-muted">` +
+      producto.cost +
+      " " +
+      producto.currency +
+      ` </small>
+                        <p class="mb-1">` +
+      producto.description +
+      `</p>
                         
                     </div>
                 </div>
             </a>
-            `
-}
-document.getElementById("produto").innerHTML = productosAMostrar;
-}
-
-`/* Ejemplo de filtrado para usar luego: data.filter(function(x){ return x.Price >= 250 && x.Price <= 800}); */`
-
-function rangoDePrecio(minRango, maxRango){
-    minRango = document.getElementById("minPrecio").value;
-    maxRango = document.getElementById("maxPrecio").value;
-    actualListadoProductos = productdata
-    arr = actualListadoProductos;
-    arr.filter(arr => minRango < arr.cost < maxRango);
-    console.log(arr);
+            `;
+  }
+  document.getElementById("produto").innerHTML = productosAMostrar;
 }
 
+`/* Ejemplo de filtrado para usar luego: data.filter(function(x){ return x.Price >= 250 && x.Price <= 800}); */`;
+
+function rangoDePrecio(minRango, maxRango) {
+  minRango = document.getElementById("minPrecio").value;
+  maxRango = document.getElementById("maxPrecio").value;
+  actualListadoProductos = productdata;
+  arr = actualListadoProductos;
+  arr.filter((arr) => minRango < arr.cost < maxRango);
+  console.log(arr);
+}
