3f194892114019e3e763bb41c0d62a6420da8069
Federico Andrés Pereira Benavente <f_p_b@hotmail.es>
2021-09-03 21:34:49 -0300

    Commit solo con formato.

--- js/products.js
+++ js/products.js
@@ -16,39 +16,24 @@ function mostrarProductos(productdata) {
   for (let i = 0; i < productdata.length; i++) {
     let producto = productdata[i];
 
-    productosAMostrar +=
-      `
-            <a href="product-info.html" class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` +
-      producto.imgSrc +
-      `" alt="` +
-      producto.description +
-      `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">` +
-      producto.name +
-      `</h4>
-                            <small class="text-muted">` +
-      producto.soldCount +
-      ` artículos</small>
-                        </div>
-                            <small class="text-muted">` +
-      producto.cost +
-      " " +
-      producto.currency +
-      ` </small>
-                        <p class="mb-1">` +
-      producto.description +
-      `</p>
-                        
-                    </div>
+    productosAMostrar += `
+    <a href="product-info.html" class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col-3">
+                <img src="` + producto.imgSrc + `" alt="` + producto.description + `" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">`+ producto.name +`</h4>
+                    <small class="text-muted">` + producto.soldCount + ` artículos</small>
                 </div>
-            </a>
-            `;
+                    <small class="text-muted">` + producto.cost + " " + producto.currency + ` </small>
+                <p class="mb-1">` + producto.description + `</p>
+                
+            </div>
+        </div>
+    </a>
+    `;
   }
   document.getElementById("produto").innerHTML = productosAMostrar;
 }
